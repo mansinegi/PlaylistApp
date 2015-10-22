7d2591feb08942512c609c6c7cc5b0ec4ccaaaf0
Mansi Negi <mansi.negi05@gmail.com>
2015-10-21 21:52:47 -0700

    server side

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
